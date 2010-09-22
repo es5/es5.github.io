7685ffc0a7c3abc72636a69ed4ee860220bc46f6
Michael(tm) Smith <mike@w3.org>
2010-09-22 09:49:27 +0900

    use circled letters for nav

--- anno.js
+++ anno.js
@@ -15,19 +15,19 @@ function xhrAnnoShow(node, panelDiv, annoClicked) {
     while (networkStatus.firstChild) networkStatus.removeChild(networkStatus.firstChild);
     networkStatus.textContent = dots;
     if (request.readyState == 4) {
-      // panelDiv.innerHTML = request.responseText;
-      if (request.status == 200) {
-        panelDiv.innerHTML = request.responseText;
-      } else {
-        var section = node.parentNode.id.substring(0,1) == "x" ? node.parentNode.id.substring(1) : node.parentNode.id;
-        if (annoClicked) {
-          panelDiv.innerHTML = "<p class='nope'>There aren't any annotations for section <i>"+section+"</i> yet…</p>"
-          + "<p>If you’d like to contribute annotations, see the "
-          + "<a href='http://sideshowbarker.github.com/es5-spec/README.html#contributing'>instructions on how to do so</a>.</p>";
-        } else {
-          panelDiv.innerHTML = "<p class='nope'>There are no errata for section <i>"+section+"</i>.</p>";
-        }
-      }
+      panelDiv.innerHTML = request.responseText;
+      // if (request.status == 200) {
+        // panelDiv.innerHTML = request.responseText;
+      // } else {
+        // var section = node.parentNode.id.substring(0,1) == "x" ? node.parentNode.id.substring(1) : node.parentNode.id;
+        // if (annoClicked) {
+          // panelDiv.innerHTML = "<p class='nope'>There aren't any annotations for section <i>"+section+"</i> yet…</p>"
+          // + "<p>If you’d like to contribute annotations, see the "
+          // + "<a href='http://sideshowbarker.github.com/es5-spec/README.html#contributing'>instructions on how to do so</a>.</p>";
+        // } else {
+          // panelDiv.innerHTML = "<p class='nope'>There are no errata for section <i>"+section+"</i>.</p>";
+        // }
+      // }
     }
   };
   try {
@@ -74,6 +74,8 @@ function annoShow(event) {
     win.document.documentElement.firstChild.appendChild(winTitle);
     var annoBody = win.document.importNode(document.getElementById("annotation"),true);
     win.document.body.appendChild(annoBody);
+    annotations.removeChild(annoPanel);
+    annoPanel = null;
     return;
   }
   if (annoPanel) {
