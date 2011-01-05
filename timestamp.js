9ddbc7166eec0a43d2ed8397c8626204c02ea70c
Michael(tm) Smith <mike@w3.org>
2011-01-05 15:53:23 +0900

    switch to github JSONP support to add timestamp

--- timestamp.js
+++ timestamp.js
@@ -1,22 +1,11 @@
-function addTimeStamp() {
-  var
-    span = document.getElementById("timestamp"),
-    apiUrl = "http://github.com/api/v2/json/repos/show/sideshowbarker/es5-spec",
-    apiResponse,
-    link,
-    request = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-  function handleRequest() {
-    if (request.readyState === 4 && request.status === 200) {
-      apiResponse = JSON.parse(request.responseText);
-      console.log(apiResponse.repository.pushed_at);
-      link = document.createElement("a");
-      link.setAttribute("href", "https://github.com/sideshowbarker/es5-spec");
-      link.textContent = "Last updated: " +  apiResponse.repository.pushed_at.substring(0, 10);
-      span.appendChild(link);
-    }
-  }
-  request.open('GET', apiUrl, true);
-  request.send(null);
-  request.onreadystatechange = handleRequest;
+function addTimestamp() {
+  var link = document.createElement("A"),
+      script = document.createElement('SCRIPT');
+  script.src = "http://github.com/api/v2/json/repos/show/sideshowbarker/es5-spec?callback=jsonp";
+  document.body.appendChild(script);
+  document.getElementById("timestamp").appendChild(link);
+  window.jsonp = function (data) {
+    link.textContent = "Last updated: " + data.repository.pushed_at.substring(0, 10);
+  };
 }
-window.addEventListener('load', addTimeStamp, false);
+window.addEventListener('load', addTimestamp(), false);
