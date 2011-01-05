3086a10b259861f309ad680cd848d908bc2f164b
Michael(tm) Smith <mike@w3.org>
2011-01-05 16:19:31 +0900

    fixed script thinko

--- timestamp.js
+++ timestamp.js
@@ -2,6 +2,7 @@ function addTimestamp() {
   var link = document.createElement("A"),
       script = document.createElement('SCRIPT');
   script.src = "http://github.com/api/v2/json/repos/show/sideshowbarker/es5-spec?callback=jsonp";
+  link.href = "https://github.com/sideshowbarker/es5-spec";
   document.body.appendChild(script);
   document.getElementById("timestamp").appendChild(link);
   window.jsonp = function (data) {
